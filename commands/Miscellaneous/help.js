7c2bdbf7e524d5a57aec3f9dd2870a56ede33446
Glitch (hello-express) <none>
2019-11-17 05:20:11 +0000

    🌠💇 Checkpoint ./commands/Miscellaneous/help.js:7551020/775

--- commands/Miscellaneous/help.js
+++ commands/Miscellaneous/help.js
@@ -17,7 +17,6 @@ const { stripIndents } = require("common-tags");
     let embed = new RichEmbed()
       .setColor(orange)
       .setAuthor(`${client.user.tag} Help`, client.user.displayAvatarURL)
-      .setThumbnail(client.user.displayAvatarURl)
     
       if(!args[0]) {
         const categories = readdirSync("./commands/")
@@ -29,7 +28,8 @@ const { stripIndents } = require("common-tags");
           const dir = client.commands.filter(c => c.config.category === category)
           const capitalise = category.slice(0, 1).toUpperCase() + category.slice(1)
             try {
-              embed.addField(`❯ ${capitalise} [${dir.size}]:`, dir.map(c => `\`${c.config.name}\``).join(" "))
+              embed.setThumbnail(client.user.displayAvatarURL)
+              embed.addField(`**❯ ${capitalise} [${dir.size}]:**`, dir.map(c => `\`${c.config.name}\``).join(" "))  
             } catch(e) {
               
             }
@@ -39,13 +39,15 @@ const { stripIndents } = require("common-tags");
         let command = client.commands.get(client.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
         if(!command) {
           embed.setTitle('Invaild Command!')
+          embed.setThumbnail(client.user.displayAvatarURL)
           embed.setDescription(`\`${args.slice(0).join(" ")}\` isn't a command. Do this command again but with no arguments to see the commands.`)
           embed.setColor(red)
           message.channel.send(embed)
         }
         command = command.config
-        
-        embed.setDescription(stripIndents `${command.name.slice(0,1).toUpperCase() + command.name.slice(1)}
+        embed.setAuthor(`${command.name.slice(0,1).toUpperCase() + command.name.slice(1)} Help`, client.user.displayAvatarURL)
+        embed.setThumbnail(client.user.displayAvatarURL)
+        embed.setDescription(stripIndents `
 
         **Command Name:** ${command.name}
         **Command Aliases:** ${command.aliases ? command.aliases.join(", ") : 'No alias.'}
