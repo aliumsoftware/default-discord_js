6480e431c2069fa69feb05ee9b4c899eb954dd57
Glitch (hello-express) <none>
2019-11-17 18:31:19 +0000

    🎣🧀 Checkpoint ./commands/Miscellaneous/userinfo.js:7551020/450

--- commands/Miscellaneous/userinfo.js
+++ commands/Miscellaneous/userinfo.js
@@ -12,7 +12,15 @@ const { orange } = require("../../colors.json")
     },
     
   run: async (client, message, args) => {
-let user = message.mentions.members.first() || message.guild.members.get(args[0])
-
+let user = message.mentions.members.first() || message.guild.members.get(args[0]);
+    // emojis
+let online = client.emojis.get('645689625591021578');
+let idle = client.emojis.get('645689592502288407');
+let dnd = client.emojis.get('645689543143718922');
+let offline = client.emojis.get('645689513590390785');
+    // if
+    
+  if(user.presence.status === 'online') user.presence.status = online
+    
     }
   }
