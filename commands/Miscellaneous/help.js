848b142b4fec686eec7cd522b44e5e012e087752
Glitch (hello-express) <none>
2019-11-17 04:56:11 +0000

    👻🚋 Checkpoint ./commands/Miscellaneous/help.js:7551020/2411

--- commands/Miscellaneous/help.js
+++ commands/Miscellaneous/help.js
@@ -13,36 +13,40 @@ const { readdirSync } = require('fs');
     },
     
   run: async (client, message, args) => {
+    let embed = new RichEmbed()
+      .setColor(orange)
+      .setAuthor(`${client.user.tag} Help`, client.user.displayAvatarURL)
+      .setThumbnail(client.user.displayAvatarURl)
     
+      if(!args[0]) {
+        const categories = readdirSync("./commands/")
+        
+        embed.setDescription(`Here are the commands for ${client.user.tag}`)
+        embed.setFooter(`Command Size: ${client.commands.size} | A Fueled Development © Project`);
+        
+        categories.forEach(category => {
+          const dir = client.cmmands.filter(c => c.config.category === category)
+          const capitalise = category.slice(0, 1).toUpperCase() + category.slice(1)
+            try {
+              embed.addField(`❯ ${capitalise} [${dir.size}]:`, dir.map(c => `\`${c.config.name}\``).join(" "))
+            } catch(e) {
+              
+            }
+        })
+        return message.channel.send(embed)
+      } else {
+        let command = client.commands.get(client.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
+        if(!command) {
+          embed.setTitle('Invaild Command!')
+          embed.setDescription(`${args.slice(0).join(" ")}`)
+        }
+      }
     }
   }
 
 
 /* 
-        let embed = new RichEmbed()
-        .setColor(gold)
-        .setAuthor(`${message.guild.me.displayName} Help`, bot.user.displayAvatarURL)
-        .setThumbnail(bot.user.displayAvatarURL)
 
-    if(!args[0]) {
-
-        const categories = readdirSync("./commands/")
-
-        embed.setDescription(`To see commands, Do ${prefix1}help_commands`)
-        embed.setFooter(`Total Commands: ${bot.commands.size} | © ${message.guild.me.displayName}`, bot.user.displayAvatarURL)
-        embed.addField(`Guild Prefix:`, `**${prefix1}**`);
-
-        categories.forEach(category => {
-            const dir = bot.commands.filter(c => c.config.category === category)
-            const capitalise = category.slice(0, 1).toUpperCase() + category.slice(1)
-            try {
-                embed.addField(`❯ ${capitalise} [${dir.size}]:`, dir.map(c => `\`${c.config.name}\``).join(" "))
-            } catch(e) {
-                
-            } 
-        }) 
-
-        return message.channel.send(embed)
     } else {
         let command = bot.commands.get(bot.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
         if(!command) return message.channel.send(embed.setTitle("Invalid Command!").setDescription(`Do \`${prefix1}help_commands\` for the list of the commands.`).setThumbnail("https://cdn.discordapp.com/attachments/612020606480678968/625104289353826329/648903-star-ratings-512.png").setColor(red_light))
