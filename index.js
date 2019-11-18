d9cdabeb65f17c0507c6d30360f9a4388fcfbea1
Glitch (hello-express) <none>
2019-11-18 02:11:17 +0000

    🏬🦄 Checkpoint ./index.js:7551020/167

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const client = new Client();
   ["aliases", "commands"].forEach(x => (client[x] = new Collection()));
   ["command", "events"].forEach(x => require(`./handlers/${x}`)(client));
 
-client.login(process.env.DTOKEN)
+client.login(process.env.DTOKEN);
+require("http").createServer(async (req,res) => { res.statusCode = 200; res.write("Oh hi, not sure how you got to this page but okay.."); res.end(); }).listen(3000);
