71cb328652679c4adf58c12ccce0be591b93f01c
Glitch (hello-express) <none>
2019-11-18 00:38:16 +0000

    🍄🎟 Checkpoint ./commands/Miscellaneous/coinflip.js:7551020/1254 ./commands/Miscellaneous/avatar.js:7551020/225

---
+++ commands/Miscellaneous/avatar.js
@@ -0,0 +1,10 @@
+const { RichEmbed } = require("discord.js");
+const { orange } = require("../../colors.json");
+
+  module.exports = {
+    config: {
+      name: 'avatar',
+      aliases: ['av', 'pfp', 'profilepic'],
+      usage: ''
+    }
+  }
--- commands/Miscellaneous/coinflip.js
+++ commands/Miscellaneous/coinflip.js
@@ -1,2 +1,23 @@
 const { RichEmbed } = require("discord.js");
-cosnt 
+const { orange } = require("../../colors.json");
+
+  module.exports = {
+    config: {
+      name: 'coinflip',
+      aliases: ['cf', 'flipacoin'],
+      usage: '^coinflip',
+      description: 'Flips a coin',
+      category: 'Miscellaneous',
+      accessableby: 'Users'
+    },
+    
+  run: async (client, message, args) => {
+  let coinSides = ['Heads', 'Tails', 'Tails', 'Heads'];
+  let final = coinSides[Math.floor(Math.random() * coinSides.length)];
+  let embed = new RichEmbed()
+    .setColor(orange)
+    .setAuthor('I flipped a coin, and got:', 'https://cdn.discordapp.com/attachments/645729612122292247/645783708590276609/free-coin-icon-794-thumb.png')
+    .setDescription(final)
+  message.channel.send(embed)
+    }
+  }
