6e8cd67db0345a34a1754fe9419ea39089bea286
Glitch (hello-express) <none>
2019-11-18 21:18:40 +0000

    🚔🌃 Checkpoint ./commands/Miscellaneous/uptime.js:7551020/2

--- commands/Miscellaneous/uptime.js
+++ commands/Miscellaneous/uptime.js
@@ -17,7 +17,7 @@ const { RichEmbed } = require("discord.js");
     const min = Math.floor((ms / (1000 * 60)) % 60).toString()
     const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
     const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
-      return `${days.padStart(1, '0')}d ${hrs.padStart(2, '0')}h ${min.padStart(2, '0')}h ${sec.padStart(2, '0')}s`
+      return `${days.padStart(1, '0')}d ${hrs.padStart(2, '0')}h ${min.padStart(2, '0')}m ${sec.padStart(2, '0')}s`
       }
     let embed = new RichEmbed()
       .setColor(orange)
