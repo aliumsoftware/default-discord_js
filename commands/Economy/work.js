0b0916c44100ffbafd5267d34f9ea847e028116e
Glitch (hello-express) <none>
2019-11-18 02:00:16 +0000

    😺🏹 Checkpoint ./commands/Economy/work.js:7551020/547

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -1,7 +1,7 @@
 const ms = require("parse-ms");
 const db = require("quick.db");
 const { RichEmbed } = require("discord.js");
-const { orange } = require("../../colors.json");
+const { orange, red } = require("../../colors.json");
 
   module.exports = {
     config: {
@@ -16,17 +16,23 @@ const { orange } = require("../../colors.json");
   run: async (client, message, args) => {
 let usrBalance = await db.fetch(`usrCash_${message.author.id}`);
   if(usrBalance === null) usrBalance = 0;    
-let timeout = 1800000
+let timeout = 14400000 // 14,400,000
 
-  
+let randomizer = Math.floor(Math.random() * 1000) + 1;
 let hour4 = await db.fetch(`hour4_${message.author.id}`);
   if(hour4 !== null && timeout - (Date.now() - hour4) > 0) {
 let time = ms(timeout - (Date.now() - hour4))
 let embed = new RichEmbed()
-  .setColor(orange)
-  .setDescription(``)
-        
-        }
-    
+  .setColor(red)
+  .setDescription(`You can work again in **${time.hours}h, ${time.minutes}m, and ${time.seconds}s**.`)
+return message.channel.send(embed)
+      } else {
+db.set(`hour4_${message.author.id}`, Date.now())
+db.add(`usrCash_${message.author.id}`, randomizer)
+  let e = new RichEmbed()
+    .setColor(orange)
+    .setDescription(`You earned a **${randomizer} ₪** paycheck.`)
+  return message.channel.send(e)
+      }
     }
   }
