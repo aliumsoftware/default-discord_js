8c2c57d916a8d7e75b1370a8ddad4851f989c00a
Glitch (hello-express) <none>
2019-11-19 03:16:00 +0000

    🐁⚡️ Checkpoint ./commands/Music/queue.js:7551020/1630

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -12,26 +12,27 @@ const { orange, red } = require("../../colors.json");
     },
     
   run: async (client, message, args, ops) => {
+let fetched = ops.active.get(message.guild.id);
+  if(!fetched) {
+    let embed = new RichEmbed()
+      .setColor(red)
+      .setDescription(`${client.emojis.get('645467660229935135')} There is currently no queue running for this guild.`)
+    return message.channel.send(embed)
+      }
+  let queue = fetched.queue;
+  let np = queue[0];
     
+    let res = ``;
+    
+      for ( var i = 1; i < queue.length; i++) {
+        res += `**__[${i}]__** ${queue[i].songTitle} **|** ${queue[i].requester}\n`
+      }
+    let embed = new RichEmbed()
+      .setColor(orange)
+      .setDescription(`**Now playing:** \`${np.songTitle}\` **|** ${np.requester}\n
+
+      **__[Queue:]__**\n${res}`)
+      //.addField('[**__Queue:__**]', `${res}`, true)
+    return message.channel.send(embed)
     }   
   }
-
-/*   let fetched = ops.active.get(message.guild.id);
-    if(!fetched) return message.channel.send('No queue running in the guild');
-  
-  let queue = fetched.queue;
-  let nowPlaying = queue[0];
-  //let resp = `__**Now Playing**__\n *${nowPlaying.songTitle}* | ${nowPlaying.requester}\n\n**Queue**\n`
-  let resp = ``;
-  
-    for ( var i = 1; i < queue.length; i++) {
-      resp += `${i}. **${queue[i].songTitle}** | ${queue[i].requester}\n`;
-    }
-    let embed = new RichEmbed()
-      .setColor(blue)
-      .setTitle(`**${message.guild.name}'s Queue**`)
-      .setDescription(`**__Now Playing:__** ${nowPlaying.songTitle} | ${nowPlaying.requester}
-
-      **Queue:**
-      ${resp}`)
-    message.channel.send(embed)*/
