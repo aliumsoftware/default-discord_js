eb949e40c2acdd85b84bdbefb2d8c73918512715
Glitch (hello-express) <none>
2019-11-19 04:15:00 +0000

    🎒🌻 Checkpoint ./commands/Miscellaneous/basicinfo.js:7551020/1157 ./package.json:7551020/4851

--- commands/Miscellaneous/basicinfo.js
+++ commands/Miscellaneous/basicinfo.js
@@ -1,5 +1,8 @@
 const { RichEmbed } = require("discord.js");
 const { orange } = require("../../colors.json");
+const cpuStat = require("cpu-stat");
+const envinfo = require("envinfo");
+const os = require('os');
 
   module.exports = {
     config: {
@@ -12,7 +15,21 @@ const { orange } = require("../../colors.json");
     },
     
   run: async (client, message, args) => {
+    cpuStat.usagePercent(async function(err, percent, seconds) {
+      var p = percent
+    var t = p.toString().slice(0, 3)
+    const inf = await envinfo.run({
+        System: ['OS', 'CPU', 'Memory', 'Shell']
+      }, { json: true})
+      let { System } = JSON.parse(inf)
     let embed = new RichEmbed()
       .setColor(orange)
+      .setTitle(`${client.emojis.get('646198812435546142')} **Client Information**`)
+      .addField('[**__Theme:__**]', '`Orange v2.1 (#eb5931)`', true)
+      .addField('[**__Client ID:__**]', `\`${client.user.id}\``, true).addField('[**__Version:__**]', '`v2`', true)
+      .addField('[**__Memory Usage:__**]', `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} mb/s\``, true)
+      .addField('[**__CPU Usage:__**]', `\`${t}\``, true).addField('[**__OS:__**]', `\`${System.OS}\``, true)
+    message.channel.send(embed)
+    })
     }
   }
