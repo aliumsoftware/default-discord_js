2c38c769ec2f3f492d8893e875547c9d1e0ee84e
Glitch (hello-express) <none>
2019-11-19 04:39:00 +0000

    🏡🍫 Checkpoint ./package.json:7551020/1712 ./commands/Miscellaneous/cat.js:7551020/912 ./commands/Miscellaneous/dog.js:7551020/916

---
+++ commands/Miscellaneous/cat.js
@@ -0,0 +1,25 @@
+const { RichEmbed } = require("discord.js");
+const { orange } = require("../../colors.json");
+const f = require('node-fetch');
+
+  module.exports = {
+    config: {
+    name: 'cat',
+    aliases: ['kitty', 'kit', 'catty-cat', 'kitten'],
+    usage: ['^cat'],
+    description: 'Sends a cat picture',
+    category: 'Miscellaneous',
+    accessableby: 'Users'
+    },
+    
+  run: async (client, message, args) => {
+    f('http://aws.random.cat/meow')
+      .then(res => res.json()).then(body => {
+          if(!body) return message.channel.send('Image couldn\'t be received. Try again.')
+        let cEmbed = new RichEmbed()
+          .setColor(orange)
+          .setImage(body.file)
+        message.channel.send(cEmbed)
+      })
+    }
+  }
---
+++ commands/Miscellaneous/dog.js
@@ -0,0 +1,25 @@
+const { RichEmbed } = require("discord.js");
+const { orange } = require("../../colors.json");
+const f = require("node-fetch");
+
+  module.exports = {
+    config: {
+    name: 'dog',
+    aliases: ['doggo', 'puppy', 'pupers', 'doggy'],
+    usage: ['^dog'],
+    description: 'Sends a dog picture',
+    category: 'Miscellaneous',
+    accessableby: 'Users'
+    },
+
+  run: async (client, message, args) => {
+    f('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json()).then(body => {
+        if(!body) return message.channel.send('Image couldn\'t be received. Try again.')
+      let dEmbed = new RichEmbed()
+        .setColor(orange)
+        .setImage(body.message)
+      message.channel.send(dEmbed)
+      })
+    }
+  }
