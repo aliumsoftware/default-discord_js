2dd44f44d223c152745463a54e231ca205dc408d
Glitch (hello-express) <none>
2019-11-20 00:29:18 +0000

    🚎🍩 Checkpoint ./commands/Music/loop.js:7551020/1528

---
+++ commands/Music/loop.js
@@ -0,0 +1,40 @@
+const { RichEmbed } = require("discord.js");
+const { orange, red } = require("../../colors.json")
+
+  module.exports = {
+    config: {
+    name: "looop",
+    aliases: ["l", "repeat"],
+    usage: ["^resume"],
+    description: "Resumes the music",
+    category: "Music",
+    accessableby: 'Users'
+    },
+    
+  run: async (client, message, args, ops) => {
+  let fetched = ops.active.get(message.guild.id)
+    if(!fetched) {
+      let embed = new RichEmbed()
+      .setColor(red)
+      .setDescription(`${client.emojis.get('645467660229935135')} There is currently no queue running for this guild.`)
+    return message.channel.send(embed)
+      }
+    if(message.member.voiceChannel !== message.guild.me.voiceChannel) {
+      let embed = new RichEmbed()
+        .setColor(red)
+        .setDescription(`${client.emojis.get('645467660229935135')} Connect to the same voicechannel as me to use this command.`)
+      return message.channel.send(embed)
+    }
+    if(!fetched.dispatcher.looped) {
+      let already = new RichEmbed()
+        .setColor(orange)
+        .setDescription(`${client.emojis.get('645467660229935135')} The music is already playing.`)
+      return message.channel.send(already)
+    }
+    fetched.dispatcher.loop()
+    let pausedS = new RichEmbed()
+      .setColor(orange)
+      .setDescription(`${client.emojis.get('645467627048665099')} Music has been looped.`)
+    return message.channel.send(pausedS)
+    }
+  }
