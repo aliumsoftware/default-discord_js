2ec5db618951d5ac70c4db9854993946de24f9de
Glitch (hello-express) <none>
2019-11-23 16:54:47 +0000

    🌧📈 Checkpoint ./commands/Miscellaneous/help.js:7551020/266

--- commands/Miscellaneous/help.js
+++ commands/Miscellaneous/help.js
@@ -2,6 +2,7 @@ const { RichEmbed } = require("discord.js");
 const { red, orange } = require("../../colors.json");
 const { readdirSync } = require('fs');
 const { stripIndents } = require("common-tags");
+const db = require('quick.db');
 
   module.exports = {
     config: {
@@ -14,6 +15,7 @@ const { stripIndents } = require("common-tags");
     },
     
   run: async (client, message, args) => {
+    let pr = await db.fetch(`prefix_${message.guild.id}`);
     let embed = new RichEmbed()
       .setColor(orange)
       .setAuthor(`${client.user.tag} Help`, client.user.displayAvatarURL)
@@ -27,6 +29,7 @@ const { stripIndents } = require("common-tags");
           const dir = client.commands.filter(c => c.config.category === category)
           const capitalise = category.slice(0, 1).toUpperCase() + category.slice(1)
             try {
+              embed.setDescription(`Guild Prefix: \`${pr}\``)
               embed.setThumbnail(client.user.displayAvatarURL)
               embed.addField(`**❯ ${capitalise} [${dir.size}]:**`, dir.map(c => `\`${c.config.name}\``).join(":"))  
             } catch(e) {
@@ -37,6 +40,7 @@ const { stripIndents } = require("common-tags");
       } else {
         let command = client.commands.get(client.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
         if(!command) {
+          embed.setAuthor('')
           embed.setTitle('Invaild Command!')
           embed.setThumbnail(client.user.displayAvatarURL)
           embed.setDescription(`\`${args.slice(0).join(" ")}\` isn't a command. Do this command again but with no arguments to see the commands.`)
@@ -46,6 +50,8 @@ const { stripIndents } = require("common-tags");
         command = command.config
         embed.setAuthor(`${command.name.slice(0,1).toUpperCase() + command.name.slice(1)} Help`, client.user.displayAvatarURL)
         embed.setDescription(stripIndents `
+
+        Guild Prefix: \`${pr}\`
 
         **Command Name**: ${command.name}
         **Command Aliases**: ${command.aliases ? `\`${command.aliases.join(", ")}\`` : 'No alias.'}
