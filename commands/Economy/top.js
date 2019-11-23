5e84efcc8034a492d13fef0177316fbd290caef2
Glitch (hello-express) <none>
2019-11-23 22:42:43 +0000

    🚶🍐 Checkpoint ./commands/Economy/top.js:7551020/1292

---
+++ commands/Economy/top.js
@@ -0,0 +1,39 @@
+const { RichEmbed } = require('discord.js');
+const db = require('quick.db');
+const { orange } = require('../../colors.json');
+
+  module.exports = {
+    config: {
+      name: 'leaderboard',
+      aliases: ['top', 'lb'],
+      usage: '^leaderboard',
+      description: 'Displays the richest users in the server',
+      category: 'Economy',
+      accessableby: 'Users'
+    },
+    
+  run: async (client, message, args) => {
+let usrAmt = db.startsWith(`usrCash_${message.author.id}`, { sort: '.data'})
+let content = '';
+    
+    for ( let i = 0; i < usrAmt.length; i++) {
+      let usr = client.users.get(usrAmt[i].ID.split('_')[2]).username
+      
+      content += `${i+1}. ${usr} ~ ${usrAmt[i].data}\n`
+      }
+    
+    return message.channel.send(content)
+    }
+  }
+
+/*    let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data'})
+    let content = "";
+
+    for (let i = 0; i < money.length; i++) {
+        let user = bot.users.get(money[i].ID.split('_')[2]).username
+
+      
+
+        content += `${i+1}. ${user} ~ ${money[i].data}\n`
+    
+      }*/
