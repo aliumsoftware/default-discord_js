8f97fc8aad3d2ea53a158ca152647f6eeb3c9461
Glitch (hello-express) <none>
2019-11-23 16:30:47 +0000

    💕🎤 Checkpoint ./commands/Miscellaneous/fortnite.js:7551020/1873

--- commands/Miscellaneous/fortnite.js
+++ commands/Miscellaneous/fortnite.js
@@ -1,7 +1,56 @@
-const { RichEmbed } = require("discord.js")
+const { RichEmbed } = require('discord.js');
+const { orange, red } = require('../../colors.json');
+const ftn = require('simple-fortnite-api'), bot = new ftn(process.env.APIFKEY);
+
+  module.exports = {
+    config: {
+      name: 'fortnite',
+      aliases: ['ftn', 'fnight', 'fortfuck'],
+      usage: '^fortnite (user) (lifetime | solo | duo | squad)',
+      description: 'Checks a user\'s fortnite stats.',
+      category: 'Miscellaneous',
+      accessableby: 'Users'
+    },
+    
+  run: async (client, message, args) => {
+const embed = new RichEmbed()
+  if(!args[0]) {
+    embed.setColor(red)
+    embed.setDescription('Who would you like me to look up?')
+    return message.channel.send(embed);
+      };
+    
+  if(!args[0] && !['lifetime', 'solo', 'duo', 'squad'].includes(args[1])) {
+    embed.setColor(red)
+    embed.setDescription('What gamemode would you like me to search that user for?')
+    return message.channel.send(embed);
+      };
+    
+  let gameType = args[1] ? args[1].toLowerCase() : 'lifetime';
+  let data = await bot.find(args[0]);
+    if(data && data.code === 404) {
+      embed.setColor(red)
+      embed.setDescription(`${client.emojis.get('645467660229935135')} No user with the username of: \`${args.join(' ')}\``)
+      return message.channel.send(embed);
+      };
+    const { image, url, username } = data;
+    const { scorePerMin, winPercent, kills, score, wins, kd, matches } = data[gameType];
+    
+    const finalEmbed = new RichEmbed()
+      .setColor(orange)
+      .setAuthor(`${username}'s Fortnite User Information.`, image)
+      .setThumbnail(image)
+      .setDescription(`**__User Statisics__**
+      
+`)
+    }
+  }
+
+
+/*const { RichEmbed } = require("discord.js")
 const { blue } = require("../../colors.json")
 const { stripIndents } = require("common-tags")
-const fornite = require("simple-fortnite-api"), client = new fornite(process.env.FORTNITETOKEN)
+const fornite = require("simple-fortnite-api"), client = new fornite(process.env.APIFKEY)
 
 module.exports = {
     config: {
@@ -13,7 +62,7 @@ module.exports = {
         accessableby: "Everyone"
     },
 
-    run: async (bot, message, args) => {
+    run: async (message, args) => {
     if(!args[0]) return message.channel.send("*What user do you want me to look up?*")
     if(!args[1] && !["lifetime", "solo", "duo", "squad"].includes(args[1])) return message.channel.send("*What game type type do you want me to look up with the username?*")
 let gameT = args[1] ? args[1].toLowerCase() : "lifetime";
@@ -23,7 +72,7 @@ let data = await client.find(args[0])
         const { scorePerMin, winPercent, kills, score, wins, kd, matches} = data[gameT]
 
     const embed = new RichEmbed()
-        .setColor(blue_light)
+        .setColor(blue)
         .setAuthor(`Epic Games (Fortnite) | ${username}`, image)
         .setThumbnail(image)
         .setDescription(stripIndents `User ${username}'s Forntite Statistics:
@@ -43,4 +92,4 @@ let data = await client.find(args[0])
         message.channel.send(embed)
 
     }    
-}
+} */
