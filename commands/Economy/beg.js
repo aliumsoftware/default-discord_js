3b45f789ba4a73e818d4a0a3f966b0da6d376a00
Glitch (hello-express) <none>
2019-11-24 13:41:49 +0000

    😘📌 Checkpoint ./ideas/top.txt:7551020/1074 ./commands/Economy/beg.js:7551020/361

---
+++ commands/Economy/beg.js
@@ -0,0 +1,11 @@
+const { RichEmbed } = require('discord.js');
+const { orange, red } = require('../../colors.json');
+
+  module.exports = {
+    config: {
+      name: 'beg',
+      aliases: ['gimmemoney', 'begging'],
+      usage: '^beg',
+      description: 'That\'s one way to do that.',
+    }
+  }
