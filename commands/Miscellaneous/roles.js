aa5eba2cac5b613ecf80cd1d610df35658fac5c5
Glitch (hello-express) <none>
2020-01-19 07:48:26 +0000

    🚄🐗 Checkpoint ./commands/Miscellaneous/roles.js:9066089/1728

---
+++ commands/Miscellaneous/roles.js
@@ -0,0 +1,38 @@
+const { RichEmbed } = require("discord.js");
+const { red } = require("../../colors.json");
+const cpuStat = require("cpu-stat");
+const envinfo = require("envinfo");
+const os = require('os');
+
+  module.exports = {
+    config: {
+      name: 'roles',
+      aliases: ['roles'],
+      usage: '^roles',
+      description: 'Bot\'s information',
+      category: 'Miscellaneous',
+      accessableby: 'Users'
+    },
+    
+  run: async (client, message, args) => {
+    cpuStat.usagePercent(async function(err, percent, seconds) {
+      var p = percent
+    var t = p.toString().slice(0, 5)
+    const inf = await envinfo.run({
+        System: ['OS', 'CPU', 'Memory', 'Shell']
+      }, { json: true})
+      let { System } = JSON.parse(inf)
+    let embed = new RichEmbed()
+      .setColor(red)
+      .setThumbnail(client.user.displayAvatarURL)
+      .setTitle(`**Choose What You Are Interested In**`)
+      .addField('[**__Theme:__**]', '``', true)
+      .addField('[**__Client ID:__**]', `\`${client.user.id}\``, true).addField('[**__Version:__**]', '`v2`', true)
+      .addField('[**__Memory Usage:__**]', `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / 512MB\``, true)
+      .addField('[**__CPU Usage:__**]', `\`${t}%\``, true).addField('[**__OS:__**]', `\`${System.OS}\``, true)
+      .addField('[**__Shard:__**]', '`Shard: N/A`', true)
+      .addField('[**__Voice Connections__**]', `\`${client.voiceConnections.size} vcs\``, true).addField('[**__Users | Servers__**]', `\`${client.users.size} users | ${client.guilds.size} servers\``, true)
+    message.channel.send(embed)
+    })
+    }
+  }
