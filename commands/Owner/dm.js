2faa087b12e96933d5efb81ed7853e49d68c2b9d
Glitch (aidens-bot) <none>
2020-01-21 01:26:38 +0000

    👠🛳 Checkpoint ./commands/Owner/dm.js:9066089/1669

---
+++ commands/Owner/dm.js
@@ -0,0 +1,27 @@
+const { RichEmbed } = require("discord.js")
+
+module.exports = {
+  name: "dm",
+  aliases: [],
+  description: "Hidden",
+  category: "Developers",
+  usage: ["Hidden"],
+  
+  run: async (bot, message, args) => {
+  if(message.author.id === "272809862591938570") {
+   try {
+  let user = await bot.fetchUser(args[0])
+  if(!user) return message.channel.send(`You need to supply a user Aiden.`)
+  let msg = args.slice(1).join(" ")
+  if(!msg) return message.channel.send(`I need something to send.`)
+    
+    user.send(`${msg}`)
+    message.channel.send(`Sent message to \`${user.tag}\` with id of \`${args[0]}\``)  
+   } catch(e) {
+     message.channel.send(`${e.message}`)
+   }
+    } else {
+    message.react(`❌`)
+    }
+  }
+}
