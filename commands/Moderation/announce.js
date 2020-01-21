53de6529e5dcbf5f07b62ae2794d69aa617dc21f
Glitch (aidens-bot) <none>
2020-01-21 17:42:28 +0000

    🌹😼 Checkpoint ./commands/Moderation/announce.js:9066089/162:10530703/1216

--- commands/Moderation/announce.js
+++ commands/Moderation/announce.js
@@ -6,15 +6,22 @@ const db = require("quick.db");
     config: {
       name: 'announce',
       aliases: ['announcement'],
-      usage: '!announce (whatever you want the bot to say)',
+      usage: '!announce (whatever you want the bot to announce)',
       description: 'Says what you inputted in an agrument in a predefined announcement channel',
       category: "Moderation",
       accessableby: 'Administrators'
     },
-    
+    //Alright I have to go to my next class, cya.
   run: async (client, message, args) => {
   let error = client.emojis.get(':x:');
   const embed = new RichEmbed()
+  //If you are not an Admin, you can't do it
+  if(!message.member.hasPermission(['MANAGE_MESSAGES', 'ADMINISTRATOR']) || !message.guild.owner) {
+    embed.setColor(red)
+    embed.setDescription(`You do not have the correct permissions to use this command! Must be an administrator.`)
+    return message.channel.send(embed)
+  }
+    //Set announcement channel
   if (args[0] == "set")
     {
       db.set(`dbAnnouncement_${client.guild}`, args[1]);
@@ -22,13 +29,17 @@ const db = require("quick.db");
       embed.setDescription(`Set announcement channel ID as` + args[1]);
       return message.channel.send(embed)
     }
-    else{
-  if(!message.member.hasPermission(['MANAGE_MESSAGES', 'ADMINISTRATOR']) || !message.guild.owner) {
-    embed.setColor(red)
-    embed.setDescription(`You do not have the correct permissions to use this command! Must be an administrator.`)
-    return message.channel.send(embed)
-  }
-    //Do you want me to make the Alpha bot go online? sure Alright its' up
+    //Set title of announcements
+    else if (args[0] == "title")
+      {
+        db.set(`dbAnnounceTitle_${client.guild}`, args.slice(1).join(' '));
+      embed.setColor(orange)
+      embed.setDescription(`Set announcement title as ` + args.slice(1).join(' '));
+      return message.channel.send(embed)
+      }
+    //Run announcement
+    else {
+  
   let text = args.slice(0).join(' ');
     
   if(!args[0]) {
@@ -37,8 +48,8 @@ const db = require("quick.db");
     return message.channel.send(embed);
       };
     //message.delete().catch()
-    
-    embed.setTitle(`Aiden's Lounge | Announcement`)
+    let title = await db.fetch(`dbAnnounceTitle_${client.guild}`);
+    embed.setTitle(title)
     embed.setColor(green)
     embed.setFooter(`https://invite.gg/aiden`)
     embed.setDescription(text)
