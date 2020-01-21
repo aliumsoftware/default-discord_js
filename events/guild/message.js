5fe88d55e03f153bb32ced503fb5bdcefa16bbda
Glitch (aidens-bot) <none>
2020-01-21 23:23:52 +0000

    👧🚌 Checkpoint ./events/guild/message.js:9066089/1491 ./commands/Moderation/poll.js:10530703/3550

--- events/guild/message.js
+++ events/guild/message.js
@@ -9,7 +9,7 @@ const { RichEmbed } = require('discord.js');
     const cmd = args.shift().toLowerCase();
     
     let fetched = await db.fetch(`prefix_${message.guild.id}`)
-      if(fetched === null) prefix = '^'
+      if(fetched === null) prefix = '!'
         else prefix = fetched
     
       let amt1 = Math.floor(Math.random() * 100) + 1;
@@ -26,8 +26,8 @@ const { RichEmbed } = require('discord.js');
     
     
     
-      if(message.channel.type === 'dm') return;
-      if(message.isMemberMentioned(client.user)) return message.reply(`The guild prefix is: \`${fetched || '^'}\`. You can change it with the prefix command.`)
+      if(message.channel.type === 'dm') return
+      if(message.isMemberMentioned(client.user)) return message.reply(`The guild prefix is: \`${fetched || '!'}\`. You can change it with the prefix command.`)
       if(!message.content.startsWith(prefix)) return;
       if(message.author.bot) return;
     let command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
