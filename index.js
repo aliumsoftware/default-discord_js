59a090387a0e8596c217176b789abec4da32100d
Glitch (aidens-bot) <none>
2020-01-22 16:30:23 +0000

    ⛺️🍴 Checkpoint ./index.js:9066089/333 ./watch.json:9066089/244

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const client = new Client();
 
 client.login(process.env.DTOKEN);
 require("http").createServer(async (req,res) => { res.statusCode = 200; res.write("Welcome to the Aiden discord bot!"); res.end(); });
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
