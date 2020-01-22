b2c731115ecb6e6f1506c1849d0e50dc07dff739
Glitch (aidens-bot) <none>
2020-01-22 17:06:25 +0000

    ⛽️💧 Checkpoint ./package.json:9066089/30:10530703/61 ./index.js:9066089/21

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received, Staying Online.");
+  console.log(Date.now() + " Ping Received. :D");
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
