9cffa9aa78ab11e58e591924c1ba151b1f2d6b40
Glitch (aidens-bot) <none>
2020-01-23 23:12:42 +0000

    🌷📼 Checkpoint ./index.js:9066089/2205

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Collection } = require("discord.js");
+const { Client, Collection, message } = require("discord.js");
 const client = new Client();
 
   ["aliases", "commands"].forEach(x => (client[x] = new Collection()));
@@ -18,6 +18,14 @@ app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
-
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a server.
+  console.log(
+    `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
+  )
+message.channel.send(`Hey thdere! Thank you for adding me to your Discord! Please copy this and paste this into a channel that I can see to set me up. **^prefix !**, please note the bot won't be fully set up until that command is ran and that you must have administrator permissions on this discord to run it. If you have any questions, visit https://invite.gg/aiden or https://discord.gg/RS4VceB.`)
+;
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 //"binding": "^3.0.3",
 //"condense-keys": "^2.0.0",
